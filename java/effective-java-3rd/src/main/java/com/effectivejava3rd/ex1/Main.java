6f4158cff81e7d9a572d037f37a59f17b3598c57
jongmin-dev <chungjm0711@gmail.com>
2022-07-06 10:27:47 +0900

    [test] : 문자열 검사

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,15 +1,28 @@
 package com.effectivejava3rd.ex1;
 
 
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            throw new Exception("예외 발생"); // 예외를 발생 시킴
-        } catch (Exception e) {
-            System.out.println("에러 메시지 = " + e.getMessage());
-            e.printStackTrace();
-        }
+
+        System.out.printf("str_part_equals(\"abcd\", 0, 2, \"ab\") : %d\n", str_part_equals("abcd", 0, 2, "ab"));
+        // 출력 => str_equals("abcd", 0, 2, "ab") : 1
+
+        System.out.printf("str_part_equals(\"abcd\", 1, 2, \"b\") : %d\n", str_part_equals("abcd", 1, 2, "b"));
+        // 출력 => str_equals("abcd", 1, 2, "b") : 1
+
+        System.out.printf("str_part_equals(\"abcd\", 2, 2, \"\") : %d\n", str_part_equals("abcd", 2, 2, ""));
+        // 출력 => str_equals("abcd", 2, 2, "") : 1
+
+        System.out.printf("str_part_equals(\"abcd\", 2, 4, \"cb\") : %d\n", str_part_equals("abcd", 2, 4, "cb"));
+        // 출력 => str_equals("abcd", 2, 4, "cb") : 0
+
+        System.out.printf("str_part_equals(\"abcd\", 2, 4, \"cd\") : %d\n", str_part_equals("abcd", 2, 4, "cd"));
+        // 출력 => str_equals("abcd", 2, 4, "cb") : 1
+    }
+
+    private static int str_part_equals(String str, int start, int end, String target) {
+        String substring = str.substring(start, end);
+        return substring.equals(target) ? 1 : 0;
     }
 }
 
