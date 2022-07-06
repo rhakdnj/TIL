3eb87b2e0b96db00a1788fdf6ac6c2f1f2cbc68d
jongmin-dev <chungjm0711@gmail.com>
2022-07-06 15:05:33 +0900

    [test] : Array stream 연습

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,51 +1,18 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.io.Serializable;
-import java.util.Objects;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Card c1 = new Card();
-        Card c2 = new Card();
+        String line = "Test 15 lqlq 35 bad 99999 guess 34";
 
-        System.out.println("c1.equals(c2) = " + c1.equals(c2));
-        System.out.println(c1.toString());
-        System.out.println(c2.toString());
-    }
+        int sum = Arrays
+                .stream(line.split(" "))
+                .filter((s) -> s.matches("\\d+"))  // 10 진수 정수
+                .mapToInt(Integer::parseInt)
+                .sum();
 
-    static class Card {
-        String kind;
-        int number;
-
-        Card() {
-            this("SPADE", 1);
-        }
-
-        Card(String kind, int number) {
-            this.kind = kind;
-            this.number = number;
-        }
-
-        // equals() 오버라이딩하면 hashCode()도 오버라이딩 해야한다.
-        @Override
-        public int hashCode() {
-            return Objects.hash(kind, number);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (!(obj instanceof Card)) {
-                return false;
-            }
-            Card c = (Card) obj;
-            return this.kind.equals(c.kind) && this.number == c.number;
-        }
-
-        // Object 클래스의 toString()을 오버라이딩
-        @Override
-        public String toString() {
-            return "kind: " + kind + ", number: " + number;
-        }
+        System.out.println(sum);
     }
 }
