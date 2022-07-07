56929ff7bd2e6fd9ecda70d747c033888b5b6503
jongmin-dev <chungjm0711@gmail.com>
2022-07-07 11:33:06 +0900

    [test] : 생성자 연습

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,38 +1,56 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Stream;
 
-public class Main {
+public
+class Main {
     public static void main(String[] args) {
-        String line = "Test 15 lqlq 35 bad 99999 guess 34";
+        System.out.println(new 사람());
+        // 출력 : 사람이 태어났습니다.
 
-        String[] arr = line.split(" ");
-        int sum1 = 0;
-        for (String s : arr) {
-            if (s.matches("\\d+")) {
-                sum1 += Integer.parseInt(s);
-            }
+        new 사람(10);
+        // 출력 : 태어나서부터 10살인 사람이 태어났습니다.
+
+        new 사람(20);
+        // 출력 : 태어나서부터 20살인 사람이 태어났습니다.
+    }
+
+    static class 사람 {
+        사람() {
+            System.out.println("사람이 태어났습니다.");
         }
-        System.out.println("sum1 = " + sum1);
-
-        int sum = Arrays
-                .stream(line.split(" "))
-                .filter((s) -> s.matches("\\d+"))  // 10 진수 정수
-                .mapToInt(Integer::parseInt)
-                .sum();
-
-        String s = "30 + 40 + 20 + 40 + 20 + 50 + 11 +  -6 + -100";
-
-        int sum2 = Arrays
-                .stream(s.split("\\+"))
-                .map(String::trim)
-                .filter((c) -> c.matches("\\d+") || c.matches("-\\d+"))  // 10 진수 정수
-                .mapToInt(Integer::parseInt)
-                .sum();
-
-        System.out.println("sum2 = " + sum2);
+        사람(int age) {
+            System.out.println("태어나서부터 "+ age+ "살인 사람이 태어났습니다.");
+        }
     }
 }
+
+
+
+//    String line = "Test 15 lqlq 35 bad 99999 guess 34";
+//
+//        String[] arr = line.split(" ");
+//        int sum1 = 0;
+//        for (String s : arr) {
+//            if (s.matches("\\d+")) {
+//                sum1 += Integer.parseInt(s);
+//            }
+//        }
+//        System.out.println("sum1 = " + sum1);
+//
+//        int sum = Arrays
+//                .stream(line.split(" "))
+//                .filter((s) -> s.matches("\\d+"))  // 10 진수 정수
+//                .mapToInt(Integer::parseInt)
+//                .sum();
+//
+//        String s = "30 + 40 + 20 + 40 + 20 + 50 + 11 +  -6 + -100";
+//
+//        int sum2 = Arrays
+//                .stream(s.split("\\+"))
+//                .map(String::trim)
+//                .filter((c) -> c.matches("\\d+") || c.matches("-\\d+"))  // 10 진수 정수
+//                .mapToInt(Integer::parseInt)
+//                .sum();
+//
+//        System.out.println("sum2 = " + sum2);
