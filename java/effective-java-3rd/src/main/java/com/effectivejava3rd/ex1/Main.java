b7c2ab73986ed6fe47a35392c7b1224c3a96705c
jongmin-dev <chungjm0711@gmail.com>
2022-07-07 13:27:39 +0900

    [test] : 명시적 super 호출

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -3,61 +3,27 @@ package com.effectivejava3rd.ex1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 사람객체 생성시작 ==");
         new 사람();
-        System.out.println("== 사람객체 생성끝 ==");
-
-        System.out.println("== 청둥오리객체 생성시작 ==");
-        new 청둥오리();
-        System.out.println("== 청둥오리객체 생성끝 ==");
-		/* 출력
-		== 청둥오리객체 생성시작 ==
-		생물이 생성되었습니다.
-		동물이 생성되었습니다.
-		오리가 생성되었습니다.
-		청둥오리 생성되었습니다.
-		== 청둥오리객체 생성끝 ==
-		*/
     }
 }
 
-class 생물 {
-    생물() {
-        // 이 함수는 부모 클래스 생성자를 자동으로 호출합니다.
-        System.out.println("생물이 생성되었습니다.");
+class 동물 {
+    동물(String 이름) {
+        System.out.println("동물(String 이름) 실행됨!");
+        System.out.println("이름 : " + 이름);
+    }
+    동물(String 이름, int 나이) {
+        System.out.println("동물(String 이름, int 나이) 실행됨!");
+        System.out.println("이름 : " + 이름);
+        System.out.println("나이 : " + 나이);
     }
 }
-class 동물 extends 생물 {
-    동물() {
-        // super(); // 부모 클래스의 생성자를 호출합니다. 생략가능합니다.
-        // 이 함수는 부모 클래스 생성자를 자동으로 호출합니다.
 
-        System.out.println("동물이 생성되었습니다.");
-    }
-
-}
 class 사람 extends 동물 {
-
     사람() {
-        super(); // 부모 클래스의 생성자를 호출합니다.
-        // 이 함수는 부모 클래스 생성자를 자동으로 호출합니다.
-
-        System.out.println("사람이 생성되었습니다.");
+        super("홍",22);
     }
 }
-
-class 청둥오리 extends 오리 {
-    청둥오리() {
-        System.out.println("청둥오리가 생성되었습니다.");
-    }
-}
-
-class 오리 extends 동물 {
-    오리() {
-        System.out.println("오리가 생성되었습니다.");
-    }
-}
-
 
 //    String line = "Test 15 lqlq 35 bad 99999 guess 34";
 //
