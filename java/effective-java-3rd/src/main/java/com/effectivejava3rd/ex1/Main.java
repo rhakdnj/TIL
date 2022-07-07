89bfadd8266e54aba5c17546a972d8fb765236b8
jongmin-dev <chungjm0711@gmail.com>
2022-07-07 11:46:46 +0900

    [test] : 생성자 연쇄호출 연습

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,27 +1,60 @@
 package com.effectivejava3rd.ex1;
 
 
-public
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(10, 20));
-        System.out.println(calc.add(10, 20, 30));
-        System.out.println(calc.add(10.5, 20, 30));
+        System.out.println("== 사람객체 생성시작 ==");
+        new 사람();
+        System.out.println("== 사람객체 생성끝 ==");
+
+        System.out.println("== 청둥오리객체 생성시작 ==");
+        new 청둥오리();
+        System.out.println("== 청둥오리객체 생성끝 ==");
+		/* 출력
+		== 청둥오리객체 생성시작 ==
+		생물이 생성되었습니다.
+		동물이 생성되었습니다.
+		오리가 생성되었습니다.
+		청둥오리 생성되었습니다.
+		== 청둥오리객체 생성끝 ==
+		*/
+    }
+}
+
+class 생물 {
+    생물() {
+        // 이 함수는 부모 클래스 생성자를 자동으로 호출합니다.
+        System.out.println("생물이 생성되었습니다.");
+    }
+}
+class 동물 extends 생물 {
+    동물() {
+        // super(); // 부모 클래스의 생성자를 호출합니다. 생략가능합니다.
+        // 이 함수는 부모 클래스 생성자를 자동으로 호출합니다.
+
+        System.out.println("동물이 생성되었습니다.");
     }
 
-    static class Calculator {
-        public int add(int a, int ... restArgs) {
-            int returnValue = a;
-            for (int restArg : restArgs) {
-                returnValue += restArg;
-            }
-            return returnValue;
-        }
+}
+class 사람 extends 동물 {
 
-        double add(double a, int b, int c) {
-            return a + b + c;
-        }
+    사람() {
+        super(); // 부모 클래스의 생성자를 호출합니다.
+        // 이 함수는 부모 클래스 생성자를 자동으로 호출합니다.
+
+        System.out.println("사람이 생성되었습니다.");
+    }
+}
+
+class 청둥오리 extends 오리 {
+    청둥오리() {
+        System.out.println("청둥오리가 생성되었습니다.");
+    }
+}
+
+class 오리 extends 동물 {
+    오리() {
+        System.out.println("오리가 생성되었습니다.");
     }
 }
 
