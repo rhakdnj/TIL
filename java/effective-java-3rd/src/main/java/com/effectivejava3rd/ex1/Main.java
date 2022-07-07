d564795dc409a0a49f61c7146ab2cdf10fded104
jongmin-dev <chungjm0711@gmail.com>
2022-07-07 13:33:27 +0900

    [test] : Stringbuffer

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -3,25 +3,9 @@ package com.effectivejava3rd.ex1;
 
 public class Main {
     public static void main(String[] args) {
-        new 사람();
-    }
-}
-
-class 동물 {
-    동물(String 이름) {
-        System.out.println("동물(String 이름) 실행됨!");
-        System.out.println("이름 : " + 이름);
-    }
-    동물(String 이름, int 나이) {
-        System.out.println("동물(String 이름, int 나이) 실행됨!");
-        System.out.println("이름 : " + 이름);
-        System.out.println("나이 : " + 나이);
-    }
-}
-
-class 사람 extends 동물 {
-    사람() {
-        super("홍",22);
+        StringBuffer sb = new StringBuffer("0123456");
+        sb.delete(3, 100);
+        System.out.println("sb = " + sb);
     }
 }
 
