3f40daf40f64bc588e42a86153b3dbc883a5c5d2
jongmin-dev <chungjm0711@gmail.com>
2022-07-08 13:26:38 +0900

    [test] : hash est -> iterator

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,18 +1,16 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        Iterator<Integer> iterator = list.iterator();
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        Iterator<Integer> iterator = set.iterator();
 
         while (iterator.hasNext()) {
             System.out.println("iterator.next() = " + iterator.next());
