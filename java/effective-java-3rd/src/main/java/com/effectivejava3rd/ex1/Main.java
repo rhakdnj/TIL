2b7253ff91d35e64f1381d0b5aeb09a83cbf4b50
jongmin-dev <chungjm0711@gmail.com>
2022-07-08 14:55:55 +0900

    [test] : treeSet -> 정렬되서 저장

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -4,52 +4,20 @@ package com.effectivejava3rd.ex1;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set set = new HashSet();
+        Set set = new TreeSet();
 
-        set.add("abc");
-        set.add("abc");
-        set.add(new Person("James", 21));
-        set.add(new Person("James", 21));
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int) (Math.random() * 45) + 1;
+            set.add(num);
+        }
 
         System.out.println(set);
     }
-
-    // equals() hashCode()를 오버라이딩해야 HashSet 바르게 동작
-    static class Person {
-        String name;
-        int age;
-
-        public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Person)) return false;
-            Person person = (Person) o;
-            return age == person.age && Objects.equals(name, person.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, age);
-        }
-    }
 }
-
 
 //    String line = "Test 15 lqlq 35 bad 99999 guess 34";
 //
