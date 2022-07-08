180bacdb0425cc0d885a045aafba175656cb5973
jongmin-dev <chungjm0711@gmail.com>
2022-07-08 20:17:58 +0900

    [test] : generics polynomial

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -5,17 +5,55 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Product> products = new ArrayList<>();
+        FruitBox<Fruit> fruitBox = new FruitBox<>();
+        FruitBox<Apple> aBox = new FruitBox<>();
+        // extends 가 아니여서 안됨
+//        FruitBox<Toy> toyBox = FruitBox < Toy > ();
 
-        products.add(new Tv());
-        products.add(new Audio());
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple());
+        fruitBox.add(new Banana());
 
-        for (Product product : products) {
-            System.out.println("product = " + product);
+        // fruit 과  그 자손들
+        FruitBox<? extends Fruit> fruitBox2 = new FruitBox<>();
+    }
+
+    static class Fruit implements Eatable {
+    }
+
+    static class Apple extends Fruit {
+    }
+
+    static class Banana extends Fruit {
+    }
+
+    static class Toy {
+    }
+
+    interface Eatable {
+    }
+
+    static class FruitBox<E extends Fruit & Eatable> extends Box<E> {
+    }
+
+    static class Box<E> {
+        ArrayList<E> list = new ArrayList<>();
+
+        void add(E item) {
+            list.add(item);
+        }
+
+        E get(int i) {
+            return list.get(i);
+        }
+
+        int size() {
+            return list.size();
+        }
+
+        public String toString() {
+            return list.toString();
         }
     }
 
-    static class Product {}
-    static class Tv extends Product {}
-    static class Audio extends Product {}
 }
