bf10531825878576d1adcfbe61e64dd2ac5cce92
jongmin-dev <chungjm0711@gmail.com>
2022-07-11 13:48:08 +0900

    [test] : wrapper class test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,61 +1,81 @@
 package com.effectivejava3rd.ex1;
 
 
-
 public class Main {
     public static void main(String[] args) {
-        for (Direction dir : Direction.values()) {
-            System.out.printf("%s = %d%n", dir.name(), dir.getValue());
-        }
-        Direction d1 = Direction.EAST;
-        Direction d2 = Direction.of(1);
+        Repo.save(new 사과());
+        Repo.save(new 사람());
+        Repo.save("안녕"); // "안녕" 은 String 클래스의 객체이다.
+        Repo.save(new String("안녕"));
 
-        System.out.printf("d1 = %s, %d%n", d1.name(), d1.getValue());
-        System.out.printf("d2 = %s, %d%n", d2.name(), d2.getValue());
+        // 정수 계열
+        byte b = 0; // 1바이트
+        char c = 1; // 2바이트
+        short s = 2; // 2바이트
+        int i = 3; // 4바이트
+        long l = 4; // 8바이트
 
-        System.out.println(Direction.EAST.rotate(1));
-        System.out.println(Direction.EAST.rotate(2));
-        System.out.println(Direction.EAST.rotate(-1));
-        System.out.println(Direction.EAST.rotate(-2));
-    }
+        // 실수 계열
+        // 4.1f => float 형 실수를 의미
+        float f = 3.14f; // 4바이트
+        // 3.141592 => double 형 실수를 의미
+        double d = 3.141592; // 8 바이트
 
-    enum Direction {
-        EAST(1, ">"), SOUTH(2, "V"), WEST(3, "<"), NORTH(10, "^");
+        // 논리 계열
+        boolean bl = false; // 1/8 바이트, 1비트
 
-        private static final Direction[] DIR_ARR = Direction.values();
-        private final int value;
-        private final String symbol;
-        Direction(int value, String symbol) {  // 접근 제어자 private 이 생략됨
-            this.value = value;
-            this.symbol = symbol;
-        }
+        // int 형 변수 i의 값 `3`을 객체화 하는 방법
+        Integer iObj = i;
+        // iObj 리모콘에 연결된 객체에는 3이라는 값이 있습니다.
+        // 객체 안에 3이있다고 생각하시면 됩니다.
 
-        public int getValue() {
-            return value;
-        }
-
-        public String getSymbol() {
-            return symbol;
-        }
-
-        public static Direction of(int dir) {
-            if (dir < 1 || dir > 4) {
-                throw new IllegalArgumentException("Invalid value : " + dir);
-            }
-            return DIR_ARR[dir - 1];
-        }
-
-        public Direction rotate(int num) {
-            num = num % 4;
-
-            if (num < 0) num += 4;
-
-            return DIR_ARR[(value - 1 + num)  % 4];
-        }
-
-        @Override
-        public String toString() {
-            return name() + ", " +  getSymbol();
-        }
+        Repo.save(b);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(s);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(c);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(i);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(l);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(f);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(d);
+        System.out.println("Repo.get() = " + Repo.get());
+        Repo.save(bl);
+        System.out.println("Repo.get() = " + Repo.get());
     }
 }
+
+class Repo {
+    static Object object;
+
+    static void save(Object v) {
+        System.out.println("Object 값 : " + v);
+        object = v;
+    }
+
+    static public Object get() {
+        return object;
+    }
+}
+
+class 사람 extends 동물 {
+}
+
+class 동물 extends 생물 {
+}
+
+class 식물 extends 생물 {
+}
+
+class 사과 extends 식물 {
+}
+
+// `extends Object` 는 생략가능하다.
+class 생물 extends Object {
+}
+
+// 이 세상에 있는 모든 클래스는 Object 클래스의 하위 클래스 이다.
+// 다시말하면 Object 클래스는 모든 클래스의 최초조상이다.
