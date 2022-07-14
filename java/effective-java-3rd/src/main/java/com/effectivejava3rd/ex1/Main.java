5c3347090035734da13f1f2d31fed73ffdb3f007
jongmin <chungjm0711@gmail.com>
2022-07-14 15:02:42 +0900

    [test] : stream test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,32 +1,17 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-        }
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> intStream = list.stream();  // list 를 스트림으로 변환
 
-        // list 모든 요소를 출력
-        list.forEach(i -> System.out.print(i + ", "));
-        System.out.println();
-
-        // list 에서 2 또는 3의 배수를 제거
-        list.removeIf(x -> x % 2 == 0 || x % 3 == 0);
-        System.out.println("list = " + list);
-
-        list.replaceAll(i -> i * 10);  // 모든 요소에 10을 곱한다.
-        System.out.println("list = " + list);
-
-        Map<String, String> map = new HashMap<>();
-        map.put("1", "1");
-        map.put("2", "2");
-        map.put("3", "3");
-        map.put("4", "4");
-
-        map.forEach((k, v) -> System.out.print("{" + k + ", " + v + "}, "));
+        // stream 모든 요소를 출력
+        intStream.forEach(System.out::print);  // 12345
+        intStream.forEach(System.out::print);  // err. stream closed
     }
 }
