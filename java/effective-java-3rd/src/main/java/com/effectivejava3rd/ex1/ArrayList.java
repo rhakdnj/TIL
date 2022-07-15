489c944c17e85ee2fae652784e824051467c1efa
jongmin <chungjm0711@gmail.com>
2022-07-15 10:39:04 +0900

    feat: ArrayList

---
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/ArrayList.java
@@ -0,0 +1,11 @@
+package com.effectivejava3rd.ex1;
+
+public class ArrayList {
+    private int size;
+    private int[] datum;
+
+    public ArrayList() {
+        size = 0;
+        datum = new int[2];
+    }
+}
