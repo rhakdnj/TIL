067d017960610648d11b2438c37299d1983dc3d1
jongmin <chungjm0711@gmail.com>
2022-07-15 10:38:39 +0900

    [test] : ArrayList_클래스가_존재한다

---
+++ java/effective-java-3rd/src/test/java/com/effectivejava3rd/ex1/ArrayListTest.java
@@ -0,0 +1,11 @@
+package com.effectivejava3rd.ex1;
+
+import org.junit.jupiter.api.Test;
+
+
+class ArrayListTest {
+    @Test
+    void ArrayList_클래스가_존재한다() {
+        ArrayList al = new ArrayList();
+    }
+}
