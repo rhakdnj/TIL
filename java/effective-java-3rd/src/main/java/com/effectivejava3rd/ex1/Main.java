98b3da65572fa5bb893f542e72f5872a3e41a0bf
jongmin <chungjm0711@gmail.com>
2022-07-15 16:48:10 +0900

    test: comparing, themComparing

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,17 +1,51 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        Stream<Integer> intStream = list.stream();  // list 를 스트림으로 변환
+        Stream<Student> studentStream = Stream.of(
+                new Student("이자바", 3, 300),
+                new Student("김자바", 1, 200),
+                new Student("안자바", 2, 100),
+                new Student("박자바", 1, 140),
+                new Student("소자바", 2, 190),
+                new Student("정자바", 1, 280),
+                new Student("김자바", 3, 110)
+        );
 
-        // stream 모든 요소를 출력
-        intStream.forEach(System.out::print);  // 12345
-        intStream.forEach(System.out::print);  // err. stream closed
+        studentStream.sorted(Comparator.comparing(Student::getBan)  // 반별 정렬
+                .thenComparing(Comparator.naturalOrder()))
+                .forEach(System.out::println);
+
+    }
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int ban;
+        int totalScore;
+
+        public Student(String name, int ban, int totalScore) {
+            this.name = name;
+            this.ban = ban;
+            this.totalScore = totalScore;
+        }
+
+        @Override
+        public String toString() {
+            return """
+                    [%s, %d, %d]""".formatted(name, ban, totalScore);
+        }
+
+        @Override  // 내림차순으로 정렬
+        public int compareTo(Student s) {
+            return s.totalScore - this.totalScore;
+        }
+
+        public int getBan() {
+            return ban;
+        }
     }
 }
