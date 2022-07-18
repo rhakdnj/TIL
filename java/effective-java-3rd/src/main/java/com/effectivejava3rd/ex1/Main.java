9ab18d4f455778225fa555525ef179004d34ef96
jongmin <chungjm0711@gmail.com>
2022-07-18 09:50:35 +0900

    [test] : generic test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,48 +1,43 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-
 public class Main {
     public static void main(String[] args) {
-        Stream<String[]> strArrStream = Stream.of(new String[]{"abc", "def", "ghi"},
-                new String[]{"ABC", "GHI", "JKL"});
+        Repo<Integer> a저장소1 = new Repo<Integer>();
 
-        Stream<String> strStream = strArrStream.flatMap(Arrays::stream);
+        a저장소1.setData(30);
+        int a = a저장소1.getData();
 
-        strStream.map(String::toLowerCase)
-                .distinct()
-                .sorted()
-                .forEach(System.out::println);
-        System.out.println();
+        System.out.println(a);
 
-        String[] lineArr = {
-                "Believe or not It is true",
-                "Do or do not There is no try",
-        };
+        Repo<Double> a저장소2 = new Repo<Double>();
 
-        Stream<String> lineStream = Arrays.stream(lineArr);
+        a저장소2.setData(5.5);
+        double b = a저장소2.getData();
 
-        List<String> strings = lineStream
-                .flatMap(line -> Stream.of(line.split(" +"))) // 하나 이상의 공백 정규식
-                .map(String::toLowerCase)
-                .distinct()
-                .sorted()
-                .toList();
+        System.out.println(b);
 
-        for (String string : strings) {
-            System.out.println(string);
+
+        Repo<사과> a저장소3 = new Repo<사과>();
+
+        a저장소3.setData(new 사과());
+        사과 c = a저장소3.getData();
+
+        System.out.println(c);
+    }
+
+    static class Repo<T> {
+        Object data;
+
+        T getData() {
+            return (T) data;
         }
 
-//        Stream<String[]> stream = lineStream.map(line -> line.split(" +"));
-//
-//        stream.forEach(s -> {3
-//            for (String s1 : s) {
-//                System.out.println("s1 = " + s1);
-//            }
-//        });
+        void setData(T data) {
+            this.data = data;
+        }
+    }
+    static class 사과 {
     }
 }
+
