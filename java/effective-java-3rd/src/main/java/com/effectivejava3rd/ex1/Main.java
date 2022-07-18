890ae02de88f5fd0cb6c33011bef40e52fd8ae99
jmjmjames <chungjm0711@gmail.com>
2022-07-18 16:20:59 +0900

    test: reduce test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,20 +1,49 @@
 package com.effectivejava3rd.ex1;
 
 
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
-        // 기본적으로 stream은 직렬 stream
-        IntStream.range(1, 10).sequential().forEach(System.out::print);
-        System.out.println();
-        IntStream.range(1, 10).sequential().forEachOrdered(System.out::print);
-        System.out.println();
+        String[] strings = {
+                "inheritance", "java", "lambda", "stream",
+                "optionalDouble", "IntStream", "count", "sum"
+        };
 
-        IntStream.range(1, 10).parallel().forEach(System.out::print);
-        System.out.println();
-        IntStream.range(1, 10).parallel().forEachOrdered(System.out::print);
-        System.out.println();
+        Stream.of(strings).forEach(System.out::println);
+
+        boolean noEmptyStr = Stream.of(strings).noneMatch(s -> s.length() == 0);
+        Optional<String> sWord = Stream.of(strings)
+                .filter(s -> s.charAt(0) == 's').findFirst();
+        Optional<String> sWord2 = Stream.of(strings).parallel()
+                .filter(s -> s.charAt(0) == 's').findAny();
+
+        System.out.println("noEmptyStr = " + noEmptyStr);
+        System.out.println("sWord.get() = " + sWord.get());
+        System.out.println("sWord2.get() = " + sWord2.get());
+
+        // Stream<String> -> Stream<Integer>
+        Stream<Integer> intStream = Stream.of(strings).map(String::length);
+
+        IntStream intStream1 = Stream.of(strings).mapToInt(String::length);
+        IntStream intStream2 = Stream.of(strings).mapToInt(String::length);
+        IntStream intStream3 = Stream.of(strings).mapToInt(String::length);
+        IntStream intStream4 = Stream.of(strings).mapToInt(String::length);
+
+        int count = intStream1.reduce(0, (a, b) -> a + 1);
+        int sum = intStream2.reduce(0, (a, b) -> a + b);
+
+        OptionalInt max = intStream3.reduce(Integer::max);
+        OptionalInt min = intStream4.reduce(Integer::min);
+
+        System.out.println("count = " + count);
+        System.out.println("sum = " + sum);
+        System.out.println("max = " + max.orElseGet(() -> 0));
+        System.out.println("min = " + min.orElse(0));
+
     }
 }
