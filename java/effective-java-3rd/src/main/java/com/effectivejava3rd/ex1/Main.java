00d67664246c7040ce95e3ff8e46ab981dbcb6d3
jmjmjames <chungjm0711@gmail.com>
2022-07-18 14:19:47 +0900

    test: optional lambda test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -4,20 +4,27 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] arr = null;          // 가능은 하지만 null pointer exception 발생 가능
-        int[] arr = new int[0];
-        System.out.println("arr.length = " + arr.length);
+        Optional<String> optStr = Optional.of("ABCDE");
+        Optional<Integer> optInt = optStr.map(String::length);
+        System.out.println("optStr.get() = " + optStr.get());
+        System.out.println("optInt.get() = " + optInt.get());
 
-//        Optional<String> opt1 = null;  // OK. 하지만 바람직 하지 않음
-        Optional<String> opt2 = Optional.empty();
-        System.out.println("opt = " + opt2);
-//        System.out.println("opt.get() = " + opt2.get());  // NoSuchElementException
+        Integer result1 = Optional.of("123")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt)
+                .get();
 
-        String str = "";
-        str = opt2.orElseGet(String::new);
-        System.out.println("str = " + str);  // 빈문자열
-        str = opt2.orElse("abc");  // optional에 지정된 값이 null 이면 "abc" 반환
-        System.out.println("str = " + str);  // abc
+        Integer result2 = Optional.of("")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).orElse(-1);
+
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+
+        Optional.of("456")
+                .map(Integer::parseInt)
+                .ifPresent(x -> System.out.println("x = " + x));
+
 
     }
 }
