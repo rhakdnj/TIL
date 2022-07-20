74bebccbd1c204c7b23c3c90ccdcbf4e109e758f
jmjmjames <chungjm0711@gmail.com>
2022-07-20 13:43:02 +0900

    [test] : factory pattern test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -3,59 +3,155 @@ package com.effectivejava3rd.ex1;
 
 public class Main {
     public static void main(String[] args) {
-        사람[] 사람들 = new 사람[7];
-        사람들[0] = 사람.get사람();
-        사람들[1] = 사람.get사람();
-        사람들[2] = 사람.get사람();
-        사람들[3] = 사람.get사람();
-        사람들[4] = 사람.get사람();
-        사람들[5] = 사람.get사람();
-        사람들[6] = 사람.get사람();
+        Duck a청둥오리 = DuckFactory.get("청둥");
+        Duck a흰오리 = DuckFactory.get("흰");
+        Duck a고무오리 = DuckFactory.get("고무");
+        Duck a고무2오리 = DuckFactory.get("고무2");
+        Duck a로봇오리 = DuckFactory.get("로봇");
 
-        for (Main.사람 사람 : 사람들) {
-            사람.자기소개();
-        }
+        a청둥오리.fly();
+        // 출력 : 청둥오리 : 날개로 날아갑니다.
+        a청둥오리.cry();
+        // 출력 : 청둥오리 : 꽥꽥!!
 
-		/*
-		// 출력
-		저는 1번째 사람입니다.
-		저는 2번째 사람입니다.
-		저는 3번째 사람입니다.
-		저는 3번째 사람입니다.
-		저는 3번째 사람입니다.
-		저는 3번째 사람입니다.
-		저는 3번째 사람입니다.
-		*/
+        a흰오리.fly();
+        // 출력 : 흰오리 : 날개로 날아갑니다.
+        a흰오리.cry();
+        // 출력 : 흰오리 : 꽥꽥!!
+
+        a고무오리.fly();
+        // 출력 : 고무오리 : 저는 날 수 없어요. ㅠㅠ
+        a고무오리.cry();
+        // 출력 : 고무오리 : 삑!삑!
+
+        a고무2오리.fly();
+        // 출력 : 고무오리 : 저는 날 수 없어요. ㅠㅠ
+        a고무2오리.cry();
+        // 출력 : 고무오리 : Peek!Peek!
+
+        a로봇오리.fly();
+        // 출력 : 로봇오리 : 로켓파워로 날아갑니다!!
+        a로봇오리.cry();
+        // 출력 : 로봇오리 : 기계는 울 수 없습니다.
+
+        a고무2오리.setFlyItem(new 로켓날개비행Item());
+        a고무2오리.fly();
+        // 출력 : 고무오리 : 로켓파워로 날아갑니다!!
     }
 
-    static class 사람 {
-        private static int index;
-        private static int max;
+    static class DuckFactory {
 
-        private int order;
-        private static 사람 temp;
+        public static Duck get(String kind) {
+            Duck duck = new Duck(kind);
 
-        private 사람(int order) {
-            this.order = order;
-        }
-
-        static {
-            index = 0;
-            max = 3;
-            temp = null;
-        }
-
-        public static 사람 get사람() {
-            if (index < max) {
-                index++;
-                temp = new 사람(index);
+            switch (kind) {
+                case "흰":
+                case "청둥":
+                    duck.setFlyItem(new 날개비행Item());
+                    duck.setCryItem(new 꽥CryItem());
+                    break;
+                case "고무":
+                    duck.setFlyItem(new 못나는비행Item());
+                    duck.setCryItem(new 삑CryItem());
+                    break;
+                case "고무2":
+                    duck.setFlyItem(new 못나는비행Item());
+                    duck.setCryItem(new 영어삑CryItem());
+                    break;
+                case "로봇":
+                    duck.setFlyItem(new 로켓날개비행Item());
+                    duck.setCryItem(new 기계CryItem());
+                    break;
             }
-            return temp;
+            return duck;
+        }
+    }
+
+    static class Duck {
+        private final String kind;
+        private FlyItem flyItem;
+        private CryItem cryItem;
+
+        public Duck(String kind) {
+            this.kind = kind;
         }
 
+        public void setFlyItem(FlyItem flyItem) {
+            this.flyItem = flyItem;
+        }
 
-        public void 자기소개() {
-            System.out.println("저는 %d번째 사람입니다.".formatted(order));
+        public void setCryItem(CryItem cryItem) {
+            this.cryItem = cryItem;
+        }
+
+        public void fly() {
+            flyItem.use(kind);
+        }
+
+        public void cry() {
+            cryItem.use(kind);
+        }
+    }
+
+    interface Item {
+
+        void use(String user);
+    }
+
+    interface FlyItem extends Item {
+
+    }
+
+    static class 날개비행Item implements FlyItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "날개로 날아갑니다.");
+        }
+    }
+
+    static class 못나는비행Item implements FlyItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "저는 날 수 없어요. ㅠㅠ");
+        }
+    }
+
+    static class 로켓날개비행Item implements FlyItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "로켓 파워로 날아갑니다.");
+        }
+    }
+
+    interface CryItem extends Item {
+
+    }
+
+    static class 꽥CryItem implements CryItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "꽥꽥!!");
+        }
+    }
+
+    static class 삑CryItem implements CryItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "삑삑!!");
+        }
+    }
+
+    static class 영어삑CryItem implements CryItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "Peek!Peek!");
+        }
+    }
+
+    static class 기계CryItem implements CryItem {
+        @Override
+        public void use(String user) {
+            System.out.println(user + "오리 : " + "기계는 울 수 없습니다.");
         }
     }
 }
