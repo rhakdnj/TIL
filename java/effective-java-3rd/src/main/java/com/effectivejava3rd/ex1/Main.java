87d5d3292c8d92635d03084e1faddad43cea5f00
jmjmjames <chungjm0711@gmail.com>
2022-07-25 10:33:26 +0900

    [test] : 추상 클래스 test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,28 +1,26 @@
 package com.effectivejava3rd.ex1;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Button button = new Button();
-        // button.?? // 이 1줄을 완성해주세요.(메인 클래스는 여기만 수정 가능)
-        button.setListener(new 사람());
-        button.click();
+        View.OnClickListener aOnClickListener;
+
+        // 수정가능지역 시작
+        aOnClickListener = () -> {
+            System.out.println("클릭되었다는 사실을 전달받았습니다.");
+        };
+        // 수정가능지역 끝
+
+        aOnClickListener.onClick();
+        // 출력 => 클릭되었다는 사실을 전달받았습니다.
     }
 
-    static class 사람 {
-        public void 안녕() {
-            System.out.println("Main::안녕!");
-        }
-    }
-    static class Button {
-        private Object listener;
-
-        public void setListener(Object listener) {
-            this.listener = listener;
-        }
-        public void click() {
-            ((사람) listener).안녕();
+    static class View {
+        interface OnClickListener {
+            void onClick();
         }
     }
 }
-
