78186bb4c1277901dc09febf4a794eb11c870ecd
jmjmjames <chungjm0711@gmail.com>
2022-07-26 09:27:23 +0900

    [test] : 추상 클래스 게산 test

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -6,21 +6,28 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        View.OnClickListener aOnClickListener;
+        계산기 a계산기 = new 계산기();
+        a계산기.num1 = 10;
+        a계산기.num2 = 20;
 
-        // 수정가능지역 시작
-        aOnClickListener = () -> {
-            System.out.println("클릭되었다는 사실을 전달받았습니다.");
-        };
-        // 수정가능지역 끝
+        int 결과1 = a계산기.수행((num1, num2) -> num1 + num2);
+        System.out.println(결과1); // 30
 
-        aOnClickListener.onClick();
-        // 출력 => 클릭되었다는 사실을 전달받았습니다.
+        int 결과2 = a계산기.수행((num1, num2) -> num1 - num2);
+        System.out.println(결과2); // -10
+
+        int 결과3 = a계산기.수행((num1, num2) -> num1 * num2);
+        System.out.println(결과3); // 300
     }
+    static class 계산기 {
+        int num1;
+        int num2;
 
-    static class View {
-        interface OnClickListener {
-            void onClick();
+        int 수행(식 a식) {
+            return a식.실행(num1, num2);
         }
+    }
+    interface 식 {
+        public int 실행(int num1, int num2);
     }
 }
