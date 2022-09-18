dbfca60b90a1e3ea073ee8adeb3b87caa6dc700b
jmjmjames <chungjm0711@gmail.com>
2022-09-18 12:26:33 +0900

    [Feat] 디자인 패턴

--- java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
+++ java/effective-java-3rd/src/main/java/com/effectivejava3rd/ex1/Main.java
@@ -1,58 +1,70 @@
 package com.effectivejava3rd.ex1;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        View aButton = new Button();
-
-        aButton.addOnClickListener(new 홍길동());
-        aButton.fireClickEvent();
-
-        aButton.addOnClickListener(new 홍길순());
-        aButton.fireClickEvent();
+        Activity activity = new MainActivity();
+        activity.onCreate();
     }
 
-    static class View {
-        private final List<OnClickListener> OnClickListenerList = new ArrayList<>();
+    static abstract class Activity {
+        abstract public void onCreate();
+    }
 
-        public void addOnClickListener(OnClickListener a구독자) {
-            OnClickListenerList.add(a구독자);
+    static class MainActivity extends Activity implements View.OnClickListener {
+        @Override
+        public void onCreate() {
+            System.out.println("메인 액티비티가 실행되었습니다.");
+
+            // 아래 코드가 실행되도록 해주세요.
+            View aButton = new Button(this);
+            aButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void clicked() {
+                    System.out.println("출력 : 익명내부객체는 버튼이 클릭되었다는 사실을 방금 알았습니다.!");
+                }
+            });
+
+            aButton.fireClickEvent();
+        }
+
+        @Override
+        public void clicked() {
+            System.out.println("MainActivity는 버튼이 클릭되었다는 사실을 방금 알았습니다.!");
+        }
+    }
+
+    abstract static class View {
+        private OnClickListener onClickListener;
+
+        interface OnClickListener {
+            void clicked();
+        }
+
+        void setOnClickListener(OnClickListener onClickListener) {
+            this.onClickListener = onClickListener;
         }
 
         public void fireClickEvent() {
-            System.out.println("뷰가 클릭되었습니다.");
+            System.out.println("== 버튼이 클릭되었습니다. ==");
+            System.out.println("구독자에게 알람을 합니다.");
 
-            for (OnClickListener a구독자 : OnClickListenerList) {
-                a구독자.소식받기("뷰가 클릭됨");
-            }
+            this.onClickListener.clicked();
         }
     }
 
     static class Button extends View {
+        Button(Activity activity) {
 
-    }
-
-    interface OnClickListener {
-        public void 소식받기(String msg);
-    }
-
-    static class 홍길동 implements OnClickListener {
-        @Override
-        public void 소식받기(String msg) {
-            System.out.println("홍길동이 받은 메세지 : " + msg);
         }
     }
 
-    static class 홍길순 implements OnClickListener {
+    static class 사과 implements View.OnClickListener {
+
         @Override
-        public void 소식받기(String msg) {
-            System.out.println("홍길순이 받은 메세지 : " + msg);
+        public void clicked() {
+            System.out.println("사과는 버튼이 클릭되었다는 사실을 방금 알았습니다.!");
         }
     }
-
 }
-
 
